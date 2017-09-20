fb3a4c6933c382249d2424440a26884d24f754e4
sandeepgodara <godara.sandeep@gmail.com>
2017-09-20 23:49:04 +0530

    Issue #1 resolved

--- src/main/scala/io/probs/ReverseInt.scala
+++ src/main/scala/io/probs/ReverseInt.scala
@@ -9,7 +9,7 @@ object ReverseInt {
     if(n==0)res
     else {
       val digit = n % 10
-      if (Int.MaxValue / 10  < res) 0 else
+      if (Math.abs(Int.MaxValue / 10)  < Math.abs(res)) 0 else
         rev(n / 10, res * 10 + digit)
     }
 }
