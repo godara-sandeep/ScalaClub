5defcd33956f6f0cfa3e28080a27fdaccc356614
reyasesino <reyasesino17@gmail.com>
2017-09-20 23:16:35 +0530

    Test Case failed cancel

--- src/test/scala/io/probs/ReverseIntSpec.scala
+++ src/test/scala/io/probs/ReverseIntSpec.scala
@@ -12,7 +12,8 @@ class ReverseIntSpec extends FlatSpec with Matchers{
   "Rev " should "reverse given int and result in 0 if overflows" in {
     rev(123456789) shouldEqual 987654321
     rev(1563847412) shouldEqual 0
-    rev(2147483647) shouldEqual 0
+    rev(-2147483647) shouldEqual 0
+    rev(1) shouldEqual 1
     rev(Int.MaxValue) shouldEqual 0
   }
 }
