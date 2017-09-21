83f6f3467487e0e34cb3bb0191d5f36acf2889d8
sachinvardhan <sachin.vardhan@mind-infotech.com>
2017-09-21 15:42:57 +0530

    Solution added for reverse string

--- src/test/scala/io/probs/ReverseIntSpec.scala
+++ src/test/scala/io/probs/ReverseIntSpec.scala
@@ -1,5 +1,7 @@
 package io.probs
 
+import java.time.{Duration, Instant}
+
 import org.scalatest.{FlatSpec, Matchers}
 
 /**
@@ -10,6 +12,7 @@ class ReverseIntSpec extends FlatSpec with Matchers{
   import ReverseInt._
 
   "Rev " should "reverse given int and result in 0 if overflows" in {
+    val start = Instant.now()
     rev(123456789) shouldEqual 987654321
     rev(1563847412) shouldEqual 0
     rev(-2147483647) shouldEqual 0
@@ -17,5 +20,7 @@ class ReverseIntSpec extends FlatSpec with Matchers{
     rev(-1) shouldEqual -1
     rev(-855) shouldEqual -558
     rev(Int.MaxValue) shouldEqual 0
+    val time = Duration.between(start, Instant.now())
+    Console println "Total Time by Sandeep: "+time.toNanos
   }
 }
