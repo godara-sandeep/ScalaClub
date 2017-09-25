80c57fc744ef78db342473c9ae7b25efc8781f31
reyasesino <reyasesino17@gmail.com>
2017-09-25 17:26:07 +0530

    Solution Added for finding the binomial coefficient using the top down approach.. Need to e optimized more

--- src/test/scala/io/probs/FindIndiciesSpec.scala
+++ src/test/scala/io/probs/FindIndiciesSpec.scala
@@ -9,6 +9,7 @@ class FindIndiciesSpec extends FlatSpec with Matchers  {
      val arr = Array(2, 7, 11, 15)
      val target = 9
      findTargetIndices(arr,target) shouldEqual (0,1)
-
+     Console println findTargetIndices(Array(2, 15, 11, 15,13,11),26)
+     findTargetIndices(Array(2, 15, 11, 15,13,11),26) shouldEqual (1,2)
    }
 }
